83598d2c505eee0acfe43efbcc8de6fd39a41178
Farel <farelfrr09@gmail.com>
2025-04-24 22:09:32 +0700

    update

--- contracts/Mytoken.sol
+++ contracts/Mytoken.sol
@@ -1,10 +1,29 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.0;
 
+// Import standar ERC20 dari OpenZeppelin
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Farel Token", "FRL") {
-        _mint(msg.sender, initialSupply * 10 ** decimals());
+contract MyToken is ERC20, Ownable {
+    // Inisialisasi token dengan nama dan simbol
+    constructor(uint256 initialSupply) ERC20("MyToken", "MTK") {
+        _mint(msg.sender, initialSupply);
+    }
+
+    // Fungsi mint untuk mencetak lebih banyak token
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    // Fungsi burn untuk mengurangi token
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+
+    // Fungsi untuk mengatur allowance atau persetujuan antar wallet
+    function approveSpender(address spender, uint256 amount) public returns (bool) {
+        _approve(msg.sender, spender, amount);
+        return true;
     }
 }
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,12 +2,19 @@ import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 import "@nomicfoundation/hardhat-ignition-viem";
 import "dotenv/config";
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomiclabs/hardhat-ethers";
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.28",
+    networks: {
+      localhost: {
+        url: "http://127.0.0.1:8545", // Ini adalah default URL Hardhat Network
+      },
+    },
     settings: {
       metadata: {
         bytecodeHash: "none",
